ea87c4a6f138043200b5a25e057a56554daf6d0b
Brolin Emeka <luxwashpay@protonmail.com>
2026-07-13 20:18:50 +0100

    Remove unavailable page navigation wrapper

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,16 +1,7 @@
-import TopNav from "@/components/ui/TopNav";
-import BottomTabBar from "@/components/ui/BottomTabBar";
-
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <TopNav />
-      <main className="md:pt-16 pb-20 md:pb-0">{children}</main>
-      <BottomTabBar />
-    </>
-  );
+  return children;
 }
