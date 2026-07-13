e1e56d63642fb40802a85311d368c0ce0aa8d338
Brolin Emeka <luxwashpay@protonmail.com>
2026-07-13 19:52:43 +0100

    Disable pwa build wrapper

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import withPWAInit from "next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+};
 
-export default withPWA(nextConfig);
+export default nextConfig;
